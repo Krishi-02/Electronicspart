f6e6c90c3b44b7330063b29bcd63a354b5a25c88
Krishi-02 <54003342+Krishi-02@users.noreply.github.com>
2022-04-02 11:26:56 +0530

    order model

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -43,7 +43,7 @@ const orderSchema = new mongoose.Schema({
         type: Number,
         required: true,
       },
-      imageUrl: {
+      image: {
         type: String,
         required: true,
       },
